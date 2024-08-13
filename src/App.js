3c979160017abc543b28cc29fd097671df29b4de
andygqw <andygqwang@gmail.com>
2024-08-13 14:18:24 -0700

    phone screen compatible for app.js

--- src/App.js
+++ src/App.js
@@ -8,17 +8,21 @@ import {
     Button,
     AppBar,
     Box,
+    IconButton,
+    Drawer,
+    List,
+    ListItem,
+    ListItemText
 } from '@mui/material';
-import {DarkMode, LightMode} from '@mui/icons-material';
+import {DarkMode, LightMode, Menu as MenuIcon} from '@mui/icons-material';
 import {theme} from './theme';
 import NotFoundPage from './Component/NotFoundPage';
 import LandingPage from './Component/LandingPage';
 import LoginPage from './Component/LoginPage';
 import RegisterPage from './Component/RegisterPage';
 import TextPage from './Component/TextPage';
-import './App.css';
-
 import {Route, Routes, useNavigate} from 'react-router-dom';
+import './App.css';
 
 function App() {
     const getDefaultMode = () => {
@@ -28,6 +32,7 @@ function App() {
 
     const [mode, setMode] = useState(getDefaultMode);
     const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem('token'));
+    const [drawerOpen, setDrawerOpen] = useState(false);
 
     const themeMode = useMemo(() => theme(mode), [mode]);
 
@@ -55,20 +60,20 @@ function App() {
         navigate('/');
     };
 
+    const toggleDrawer = () => {
+        setDrawerOpen(!drawerOpen);
+    };
+
+    const handleMenuClick = (path) => {
+        navigate(path);
+        setDrawerOpen(false);
+    };
+
     return (
         <ThemeProvider theme={themeMode}>
             <CssBaseline/>
-            <AppBar position="absolute"
-                    elevation={0}
-                    color="transparent"
-                    sx={{
-                        top: 0,
-                        left: 0,
-                        right: 0,
-                        pl: 2,
-                        pr: 2,
-                    }}
-            >
+            <AppBar position="absolute" elevation={0} color="transparent"
+                    sx={{top: 0, left: 0, right: 0, pl: 2, pr: 2}}>
                 <Toolbar>
                     <Typography
                         onClick={() => navigate('/')}
@@ -84,28 +89,77 @@ function App() {
                         }}>
                         GWTool
                     </Typography>
-                    <Button color="inherit" onClick={() => navigate('/text')}>Text</Button>
-                    <Button color="inherit" onClick={() => navigate('/word')}>Words</Button>
-                    <Button color="inherit" onClick={() => navigate('/file')}>Files</Button>
-                    <Button color="inherit" onClick={() => navigate('/resource')}>Resources</Button>
-                    <Button color="inherit" onClick={() => navigate('/kiskis')}>KisKis</Button>
-                    <Switch
-                        checked={mode === 'dark'}
-                        onChange={toggleTheme}
-                        icon={<LightMode sx={{color: themeMode.palette.text.primary}}/>}
-                        checkedIcon={<DarkMode sx={{color: themeMode.palette.text.primary}}/>}
-                    />
-                    {isLoggedIn ? (
-                        <Button color="inherit" onClick={handleLogout}>
-                            Logout
-                        </Button>
-                    ) : (
-                        <Button color="inherit" onClick={() => navigate('/login')}>
-                            Login
-                        </Button>
-                    )}
+                    <Box sx={{display: {xs: 'none', md: 'flex'}}}>
+                        <Button color="inherit" onClick={() => navigate('/text')}>Text</Button>
+                        <Button color="inherit" onClick={() => navigate('/word')}>Words</Button>
+                        <Button color="inherit" onClick={() => navigate('/file')}>Files</Button>
+                        <Button color="inherit" onClick={() => navigate('/resource')}>Resources</Button>
+                        <Button color="inherit" onClick={() => navigate('/kiskis')}>KisKis</Button>
+                        <Switch
+                            checked={mode === 'dark'}
+                            onChange={toggleTheme}
+                            icon={<LightMode sx={{color: themeMode.palette.text.primary}}/>}
+                            checkedIcon={<DarkMode sx={{color: themeMode.palette.text.primary}}/>}
+                        />
+                        {isLoggedIn ? (
+                            <Button color="inherit" onClick={handleLogout}>
+                                Logout
+                            </Button>
+                        ) : (
+                            <Button color="inherit" onClick={() => navigate('/login')}>
+                                Login
+                            </Button>
+                        )}
+                    </Box>
+                    <IconButton
+                        edge="end"
+                        color="inherit"
+                        aria-label="menu"
+                        onClick={toggleDrawer}
+                        sx={{display: {xs: 'flex', md: 'none'}}}
+                    >
+                        <MenuIcon/>
+                    </IconButton>
                 </Toolbar>
             </AppBar>
+
+            <Drawer anchor="right" open={drawerOpen} onClose={toggleDrawer}>
+                <List sx={{width: 250}}>
+                    <ListItem button onClick={() => handleMenuClick('/text')}>
+                        <ListItemText primary="Text"/>
+                    </ListItem>
+                    <ListItem button onClick={() => handleMenuClick('/word')}>
+                        <ListItemText primary="Words"/>
+                    </ListItem>
+                    <ListItem button onClick={() => handleMenuClick('/file')}>
+                        <ListItemText primary="Files"/>
+                    </ListItem>
+                    <ListItem button onClick={() => handleMenuClick('/resource')}>
+                        <ListItemText primary="Resources"/>
+                    </ListItem>
+                    <ListItem button onClick={() => handleMenuClick('/kiskis')}>
+                        <ListItemText primary="KisKis"/>
+                    </ListItem>
+                    <ListItem button onClick={toggleTheme}>
+                        <Switch
+                            checked={mode === 'dark'}
+                            icon={<LightMode sx={{color: themeMode.palette.text.primary}}/>}
+                            checkedIcon={<DarkMode sx={{color: themeMode.palette.text.primary}}/>}
+                        />
+                        <ListItemText primary="Toggle Theme"/>
+                    </ListItem>
+                    {isLoggedIn ? (
+                        <ListItem button onClick={handleLogout}>
+                            <ListItemText primary="Logout"/>
+                        </ListItem>
+                    ) : (
+                        <ListItem button onClick={() => handleMenuClick('/login')}>
+                            <ListItemText primary="Login"/>
+                        </ListItem>
+                    )}
+                </List>
+            </Drawer>
+
             <Box component="main" sx={{
                 height: 'auto',
                 margin: 0,
@@ -121,7 +175,7 @@ function App() {
                     <Route path="/login" element={<LoginPage setIsLoggedIn={setIsLoggedIn}/>}/>
                     <Route path="/register" element={<RegisterPage/>}/>
                     <Route path="/text" element={<TextPage/>}/>
-                    <Route path="*" element={<NotFoundPage />} />
+                    <Route path="*" element={<NotFoundPage/>}/>
                 </Routes>
             </Box>
         </ThemeProvider>
