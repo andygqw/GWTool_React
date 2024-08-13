6a923b0ee800692bbd58e1f475b75045f43162ff
andygqw <andygqwang@gmail.com>
2024-08-13 13:02:19 -0700

    Group of flaoting inputs

--- src/utils/GroupFloatingInput.js
+++ src/utils/GroupFloatingInput.js
@@ -7,6 +7,8 @@ const GroupFloatingInput = () => {
     const [input1, setInput1] = useState('GWTool');
     const [input2, setInput2] = useState('Hello World!');
     const [input3, setInput3] = useState('Welcome');
+    const [input4, setInput4] = useState('Aloha~');
+    const [input5, setInput5] = useState('Try this out');
 
     return (
 
@@ -23,8 +25,8 @@ const GroupFloatingInput = () => {
                 label="Text Submit"
                 value={input1}
                 onChange={(e) => setInput1(e.target.value)}
-                sx={{top: "15%",
-                    left: '65%',
+                sx={{top: "5%",
+                    left: '80%',
                     position: 'absolute',
                 }}
                 float="float1"
@@ -34,7 +36,7 @@ const GroupFloatingInput = () => {
                 value={input2}
                 onChange={(e) => setInput2(e.target.value)}
                 sx={{
-                    top: '45%',
+                    top: '25%',
                     left: '20%',
                     position: 'absolute',
                 }}
@@ -45,30 +47,52 @@ const GroupFloatingInput = () => {
                 value={input3}
                 onChange={(e) => setInput3(e.target.value)}
                 sx={{
-                    top: '78%',
-                    left: '25%',
+                    top: '45%',
+                    left: '75%',
                     position: 'absolute',
                 }}
                 float="float3"
+            />
+            <FloatingInput
+                label="Greetings"
+                value={input3}
+                onChange={(e) => setInput4(e.target.value)}
+                sx={{
+                    top: '65%',
+                    left: '23%',
+                    position: 'absolute',
+                }}
+                float="float2"
+            />
+            <FloatingInput
+                label="Text Field"
+                value={input3}
+                onChange={(e) => setInput5(e.target.value)}
+                sx={{
+                    top: '85%',
+                    left: '77%',
+                    position: 'absolute',
+                }}
+                float="float1"
             />
 
             <style>
                 {`
                     @keyframes float1 {
                         0% { transform: translate(0, 0); }
-                        50% { transform: translate(-5px, -15px); }
+                        50% { transform: translate(5px, 0); }
                         100% { transform: translate(0, 0); }
                     }
 
                     @keyframes float2 {
                         0% { transform: translate(0, 0); }
-                        50% { transform: translate(-10px, 5px); }
+                        50% { transform: translate(-10px, 0); }
                         100% { transform: translate(0, 0); }
                     }
 
                     @keyframes float3 {
                         0% { transform: translate(0, 0); }
-                        50% { transform: translate(5px, 10px); }
+                        50% { transform: translate(10px, 0); }
                         100% { transform: translate(0, 0); }
                     }
                 `}
